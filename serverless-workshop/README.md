34334d9fa4d898fb9bec1771299c74af4dfa69e1
flavono123 <flavono123@gmail.com>
2023-09-12 23:59:52 +0900

    feat(serverless-workshop): add iam role for api gateway

--- serverless-workshop/README.md
+++ serverless-workshop/README.md
@@ -1,3 +1,8 @@
 # Serverless Workshop
 
 [Link](https://catalog.us-east-1.prod.workshops.aws/workshops/4923c0ff-6470-46e1-9884-7c6ee63e7136/ko-KR)
+
+## 시작 전 준비 사항
+
+- [API Gateway용 IAM Role 생성](https://catalog.us-east-1.prod.workshops.aws/workshops/4923c0ff-6470-46e1-9884-7c6ee63e7136/ko-KR/start/cloudformation)
+  - CloudFormation -> HCL
--- serverless-workshop/main.tf
+++ serverless-workshop/main.tf
@@ -5,3 +5,30 @@ terraform {
     aws = "~> 5.16.2"
   }
 }
+
+# cf2tf <(wget -qO- https://static.us-east-1.prod.workshops.aws/public/ebdd2af6-e669-4dd3-99bd-c9de7921832e/assets/serverless-lab-cf.yaml)
+# wrap assume_role_policy in jsonencode
+data "aws_partition" "current" {}
+
+resource "aws_iam_role" "apigwdeliveryorderrole_f75_cf62_c" {
+  assume_role_policy = jsonencode({
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "apigateway.amazonaws.com"
+        }
+      }
+    ]
+    Version = "2012-10-17"
+  })
+
+  description = "A role for API Gateway to access SQS and EventBridge"
+  managed_policy_arns = [
+    join("", ["arn:", data.aws_partition.current.partition, ":iam::aws:policy/service-role/AmazonAPIGatewayPushToCloudWatchLogs"]),
+    join("", ["arn:", data.aws_partition.current.partition, ":iam::aws:policy/AmazonSQSFullAccess"]),
+    join("", ["arn:", data.aws_partition.current.partition, ":iam::aws:policy/AmazonEventBridgeFullAccess"])
+  ]
+  name = "apigw-delivery-order-role"
+}
