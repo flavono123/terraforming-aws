e8e6d1eaf095f0ecca6a8ff0f08c5dc251e20e8c
flavono123 <flavono123@gmail.com>
2023-09-13 16:03:26 +0900

    feat: add API GW
    - done https://catalog.us-east-1.prod.workshops.aws/workshops/4923c0ff-6470-46e1-9884-7c6ee63e7136/ko-KR/simple-api-service/create-apigw

--- serverless-workshop/main.tf
+++ serverless-workshop/main.tf
@@ -6,6 +6,13 @@ terraform {
   }
 }
 
+locals {
+  apigateway = {
+    api_name   = "delivery-api"
+    stage_name = "dev"
+  }
+}
+
 # cf2tf <(wget -qO- https://static.us-east-1.prod.workshops.aws/public/ebdd2af6-e669-4dd3-99bd-c9de7921832e/assets/serverless-lab-cf.yaml)
 # wrap assume_role_policy in jsonencode
 data "aws_partition" "current" {}
@@ -31,4 +38,25 @@ resource "aws_iam_role" "apigwdeliveryorderrole_f75_cf62_c" {
     join("", ["arn:", data.aws_partition.current.partition, ":iam::aws:policy/AmazonEventBridgeFullAccess"])
   ]
   name = "apigw-delivery-order-role"
+}
+
+## API Gateway
+
+### API
+resource "aws_apigatewayv2_api" "delivery_api" {
+  name          = local.apigateway.api_name
+  protocol_type = "HTTP"
+}
+
+
+### Deploy/Stages
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.delivery_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+resource "aws_apigatewayv2_stage" "dev" {
+  api_id      = aws_apigatewayv2_api.delivery_api.id
+  name        = local.apigateway.stage_name
+  auto_deploy = true
 }
---
+++ serverless-workshop/outputs.tf
@@ -0,0 +1,4 @@
+## API Gateway
+output "apigatewayv2_api_endpoint" {
+  value = aws_apigatewayv2_api.delivery_api.api_endpoint
+}
